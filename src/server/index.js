e67a8961e1886272166cf992c682153ce7e2bba7
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2017-03-05 23:14:43 +0100

    add nunjucks

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 import http from 'http';
+import path from 'path';
 import logger from 'morgan';
 import express from 'express';
+import nunjucks from 'nunjucks';
 import compression from 'compression';
 import { host, port, env } from 'c0nfig';
 
@@ -10,8 +12,20 @@ if ('test' !== env) {
   app.use(logger('dev'));
 }
 
+nunjucks.configure(path.join(__dirname, './views'), {
+  autoescape: true,
+  cache: false,
+  express: app
+});
+
 app.disable('x-powered-by');
 app.use(compression());
+
+app.get('/', (req, res) => {
+  res.render('index.html', {
+    title: 'r-o-b.media'
+  });
+});
 
 export function start() {
   http.createServer(app).listen(port, () => {
