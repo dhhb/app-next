5217f553a7f7e7b0fdb14b1e0470233e8df5b90c
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2017-03-13 00:34:36 +0100

    add minigrid

--- src/client/pages/home.js
+++ src/client/pages/home.js
@@ -1,3 +1,13 @@
 import './home.css';
 
-console.log('home file');
+import Minigrid from 'minigrid';
+
+const grid = new Minigrid({
+  container: '.home-articles-list',
+  item: '.home-articles-item',
+  gutter: 12
+});
+
+grid.mount();
+
+window.addEventListener('resize', () => grid.mount());
